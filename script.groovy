ed8415f9447fcd7cbcb1abdf7a6acdfcc86fb41a
Harshwardhan Patil <126240589+HarshwardhanPatil07@users.noreply.github.com>
2025-03-20 19:52:58 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: '73b703cb-6da5-46be-b448-5954f154defe', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t harshwardhan07/harshwardhan:jenkinsJMA-2.0 .'
+        sh 'docker build -t harshwardhan07/harshwardhan:jenkinsJMA-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push harshwardhan07/harshwardhan:jenkinsJMA-2.0'
+        sh 'docker push harshwardhan07/harshwardhan:jenkinsJMA-1.0'
     }
 } 
 
